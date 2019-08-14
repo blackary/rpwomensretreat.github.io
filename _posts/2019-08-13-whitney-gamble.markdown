c8401630c2e13462d6014216e01760c63d11ff70
Zachary Blackwood <zblackwo@gmail.com>
2019-08-13 21:57:36 -0400

    Switched to jekyll theme

---
+++ _posts/2019-08-13-whitney-gamble.markdown
@@ -0,0 +1,17 @@
+---
+layout: default
+modal-id: 1
+date: 2019-08-13
+img: speakers/gamble.jpeg
+alt: image-alt
+description: 'Dr. Whitney Gamble'
+
+---
+
+Dr. Whitney Gamble teaches biblical and theological studies at Providence Christian College and serves as Director of Accreditation and Assessment at The Master’s Seminary. She previously taught at the University of Edinburgh, Scotland, and at Geneva College in Beaver Falls, PA. Dr.Gamble holds a PhD in Systematic and Historical Theology from the University of Edinburgh, where she was the winner of the College Studentship, a highly-competitive tuition plus stipend grant awarded to one incoming PhD student. She earned a Master of Theological Studies from the Reformed Presbyterian Theological Seminary and a BA in Biblical Studies from Geneva College.
+
+Her first book, Christ and the Law: Antinomianism at the Westminster Assembly, was published in 2018. She has written for Modern Reformation, Reformation Renaissance & Review, Meet the Puritans, and The Gospel Coalition. Her chapter on the theology of the Westminster Confession of Faith will appear in the forthcoming Oxford University Press multi-volume set, The History of Scottish Theology. 
+
+Dr. Gamble also is contributing the chapter on John Owen’s doctrine of justification for a new T&T Clark anthology on Owen’s theology. Dr. Gamble has frequently co-hosted episodes for The White Horse Inn podcast, as well as interviewed for the Reformed Forum, the New Books Network, Pilgrim Radio, and Westminster Welcomes, Westminster Seminary California. She has given invited lectures and papers at the University of Edinburgh, Cambridge University, Durham University, Westminster Seminary California, Princeton Theological Seminary, Theologische Universiteit in the Netherlands, and the Evangelical Theological Society. In 2012 and 2013, Dr. Gamble received scholarships to study at the Institut Catholique de Paris, as well as an Erasmus grant to study at the University of Tübingen, Germany. In 2015, she was awarded a Fellowship in Advanced Theological Studies to study at the Theologische Universiteit, Kampen, the Netherlands.
+
+
--- js/contact_me.js
+++ js/contact_me.js
@@ -1,15 +1,12 @@
 $(function() {
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
+            event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -31,8 +28,7 @@ $(function() {
                 },
                 cache: false,
                 success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
+                    // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
@@ -49,12 +45,12 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            });
+            })
         },
         filter: function() {
             return $(this).is(":visible");
@@ -67,7 +63,8 @@ $(function() {
     });
 });
 
-// When clicking on Full hide fail/success boxes
+
+/*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
 });
